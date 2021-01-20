c12db4c97117a2ba9aa5223b428d000b0ec099bd
Alvin Hu <alvin.hu@gmail.com>
2021-01-20 17:39:24 +0800

    add Day1 homework

---
+++ D01/homework.py
@@ -0,0 +1,8 @@
+import numpy as py
+
+print(py.__version__)
+
+EquDifference = py.arange(0,21,1)
+print(EquDifference)
+print([ x for x in EquDifference if x%2==0])
+print([ x for x in EquDifference[1:] if x%3==0])
