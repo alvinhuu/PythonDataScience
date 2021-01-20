b32b7001b746a014d749ed683800eb848d59785e
Alvin Hu <alvin.hu@gmail.com>
2021-01-20 19:09:13 +0800

    add 作業解答

--- D01/homework.py
+++ D01/homework.py
@@ -6,3 +6,6 @@ EquDifference = py.arange(0,21,1)
 print(EquDifference)
 print([ x for x in EquDifference if x%2==0])
 print([ x for x in EquDifference[1:] if x%3==0])
+
+print(EquDifference[::2] )
+print(EquDifference[3::3] )
