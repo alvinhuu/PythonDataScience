0edbac2181f4191e5048fc7e0a6523b679cf1ac5
Alvin Hu <alvin.hu@gmail.com>
2021-01-20 19:05:38 +0800

    update day 2 homework.py

--- D02/homework.py
+++ D02/homework.py
@@ -3,7 +3,7 @@ import numpy as np
 array1 = np.array(range(30))
 a = array1.reshape((5,6), order='F')
 print(a)
-b = np.where(a%6==1,a,0)
+b = np.where(a%6==1,a," ")
 c = np.where(a%6==1)
 print(b)
 print(c)
