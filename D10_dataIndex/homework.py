891747929651beee8d572fe3162cc9d25073958b
Alvin Hu <alvin.hu@gmail.com>
2021-01-26 18:14:08 +0800

    add Day10's homework

---
+++ D10_dataIndex/homework.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# 讀取 STOCK_DAY_0050_202009.csv 串聯 STOCK_DAY_0050_202010.csv，並且找出 open 小於 close 的資料
+
+stock09 = pd.read_csv('STOCK_DAY_0050_202009.csv' )
+stock10 = pd.read_csv("STOCK_DAY_0050_202010.csv" )
+stockFin = stock09.append(stock10)
+
+# print(stock09[stock09.open<stock09.close])
+# print(stock10[stock10.open<stock10.close])
+print(stockFin[stockFin.open<stockFin.close])
