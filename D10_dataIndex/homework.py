2c98e3aaecc6b43cb4713daa5b2393101ea5221c
Alvin Hu <alvin.hu@gmail.com>
2021-01-26 18:19:23 +0800

    add website's solution

--- D10_dataIndex/homework.py
+++ D10_dataIndex/homework.py
@@ -4,8 +4,10 @@ import pandas as pd
 
 stock09 = pd.read_csv('STOCK_DAY_0050_202009.csv' )
 stock10 = pd.read_csv("STOCK_DAY_0050_202010.csv" )
+#串聯
 stockFin = stock09.append(stock10)
-
+# 答案的串聯
+#stockFin = pd.concat([stock09,stock10])
 # print(stock09[stock09.open<stock09.close])
 # print(stock10[stock10.open<stock10.close])
 print(stockFin[stockFin.open<stockFin.close])
