b3cfad338d59b7718c3cb66912f825dab647bc84
Alvin Hu <alvin.hu@gmail.com>
2021-02-17 22:01:17 +0800

    add Day20 homework

---
+++ D20_seaborn/homework.py
@@ -0,0 +1,30 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+# 比較好的教學 https://zhuanlan.zhihu.com/p/27816821
+
+# 作業目標(1)：
+# 繪製模型殘差 圖型
+
+
+#設定圖形樣式 - whitegrid
+# sns.set_style('whitegrid')
+sns.set(style='whitegrid')
+
+# 利用 NUMPY 去建立資料集 
+rs = np.random.RandomState(7)
+x = rs.normal(2, 1, 75)
+y = 2 + 1.5 * x + rs.normal(0, 2, 75) 
+# 畫圖
+sns.residplot(x=x, y=y, lowess=True, color="g")
+
+# 作業目標(2)：
+
+# 使用 distplot()使用簡單的規則來正確猜測預設情況下正確的數位,但嘗試更多或更少的 bin 可能會顯示資料中的其他特徵：
+# 有無kde對圖形分布的影響
+# bin: 指的是特徵值, 
+
+# 使用sns.distplot()
+sns.displot()
+###
+plt.show()
