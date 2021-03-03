41a7ded1c2a340d628c11c49f3a62ed54c70a8b9
Alvin Hu <alvin.hu@gmail.com>
2021-03-03 11:52:04 +0800

    update homework

--- D26_statistics/homework.py
+++ D26_statistics/homework.py
@@ -15,72 +15,96 @@ import math
 import statistics
 import seaborn as sns
 
-#輸入資料
-boys=[164, 176, 169, 169, 165, 175, 159, 151, 144, 160, 183, 165, 156, 170, 164, 173, 165, 163, 177, 171]
-print(sorted(boys))
+## 輸入資料
+boys=[164, 176, 169, 169, 165, 175, 159, 151, 144, 160, 183, 165, 156, 170,
+ 164, 173, 165, 163, 177, 171]
+
+girls=[169, 183, 170, 168, 182, 170, 173, 185, 151, 156, 162, 169, 162, 181,
+ 159, 154, 167, 175, 170, 160] 
+ #Q1:試著用今天所教的內容，如何描述這兩組資料的樣態?
+#暗示:  分別計算男生和女生的統計量
 # 計算統計量_平均數的方法
 mean_boy=np.mean(boys)
 print("男孩身高平均=",mean_boy)
-
-statistics_mean_boy=statistics.mean(boys)
-print("statistics_mean_boy=",statistics_mean_boy)
- 
+mean_girl=np.mean(girls)
+print("女孩身高平均=",mean_girl)
 
 # 計算統計量_中位數的方法
 np_median_boy=np.median(boys,axis=None)
-print("np_median_boy=",np_median_boy) 
+print("男孩身高中位數=",np_median_boy)
+np_median_girl=np.median(girls,axis=None)
+print("女孩身高中位數=",np_median_girl)
 
-statistics_median_boy=statistics.median(boys)
-print("statistics_median_boy=",statistics_median_boy)
-
-# 統計量_眾數
-# 統計量的眾數，如果有多個眾數，取最小的值當眾數。
-
+#計算統計量_眾數
 mode_boy=stats.mode(boys,axis=None)
-print("男孩身高眾數=",mode_boy)
 print("男孩身高眾數=",mode_boy[0][0])
 
-# 統計量_眾數
-statistics_mode_boy=statistics.mode(boys)
-print("statistics_mode_boy=",statistics_mode_boy)
+mode_girl=stats.mode(girls,axis=None)
+print("女孩身高眾數=",mode_girl[0][0])
 
-#全距
-#rangeV=max(boys)-min(boys)
+#計算全距:
 def rangeV(x): 
   return(max(x)-min(x))
     
-print(rangeV(boys))
+print("男孩身高全距=",rangeV(boys))
+print("女孩身高全距=",rangeV(girls))
 
-# 計算變異數的方法
-print("男孩身高變異數=",statistics.variance(boys))
+#計算變異數
 print("男孩身高變異數=",np.var(boys,ddof=1))
+print("男孩身高變異數=",np.var(girls,ddof=1))
 
-# 統計量_標準差的方法
-#樣本標準差
-#ddof=1, 回傳 sample standard deviation 樣本標準差，分母(n-1)，無偏估計
+
+#計算標準差
 std_boy=np.std(boys,ddof=1)
 print("男孩身高標準差=",std_boy)
+std_girl=np.std(girls,ddof=1)
+print("女孩身高標準差=",std_girl)
 
-statistics_stdev_boy=statistics.stdev(boys)
-print("statistics_mean_boy=",statistics_stdev_boy)
-
-# python 百分位數
+## python 百分位數
 #np
-print("90百分位數=",np.percentile(boys, 90))
-print("50百分位數=",np.percentile(boys, 50))
-print("20百分位數=",np.percentile(boys, 20))
-#stat
-print("20百分位數=",stats.scoreatpercentile(boys, 20))
+print("男孩身高90百分位數=",np.percentile(boys, 90))
+print("男孩身高50百分位數=",np.percentile(boys, 50))
+print("男孩身高20百分位數=",np.percentile(boys, 20))
+
+print("女孩身高90百分位數=",np.percentile(girls, 90))
+print("女孩身高50百分位數=",np.percentile(girls, 50))
+print("女孩身高20百分位數=",np.percentile(girls, 20))
+
 
 #計算峰度和偏度
-print(stats.skew(boys))
-print(stats.kurtosis(boys))
+print("男孩身高偏度=",stats.skew(boys))
+print("男孩身高峰度=",stats.kurtosis(boys))
 
-# pandas和 stat 接近
-# python的峰帶
+print("女孩身高偏度=",stats.skew(girls))
+print("女孩身高峰度=",stats.kurtosis(girls))
 
-#最後，畫圖看分布
+# https://kknews.cc/zh-tw/code/3ngaz5a.html
+#最後，畫圖看分布 | boy's skew and kurtosis
 plt.hist(boys,alpha=.4,bins=40)
 plt.title('boy,skewness={0},kurtosis={1}'.format(round(stats.skew(boys),2),round(stats.kurtosis(boys),2)))
 plt.axvline(x=mean_boy)
 plt.show()
+
+#最後，畫圖看分布 | girl's skew and kurtosis
+plt.hist(girls,alpha=.4,bins=40,color=sns.desaturate("indianred", .8))
+plt.title('girl,skewness={0},kurtosis={1}'.format(round(stats.skew(girls),2),round(stats.kurtosis(girls),2)))
+plt.axvline(x=mean_girl,color=sns.desaturate("indianred", .8))
+plt.show()
+
+plt.hist(boys,alpha=.4)
+plt.hist(girls,color=sns.desaturate("indianred", .8),alpha=.4)
+plt.title("all samples")
+plt.axvline(x=mean_girl,color=sns.desaturate("indianred", .8))
+plt.axvline(x=mean_boy)
+plt.show()
+
+#Q1答案: 所以從分布看的出來，女生的平均身高較高，且資料較為集中，男生的資料較為分散。
+
+#Q2: 請問男生和女生在平均身高上誰比較高? 
+# 計算統計量_平均數的方法
+mean_boy=np.mean(boys)
+print("男孩身高平均=",mean_boy)
+mean_girl=np.mean(girls)
+print("女孩身高平均=",mean_girl)
+#女生平均而言比較高
+
